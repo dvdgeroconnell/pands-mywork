fb8c802abc726bfe59b9404c765891be0ff23099
David O'Connell <dvdgeroconnell@gmail.com>
2024-03-24 13:51:08 +0000

    Commit lab work for wk9 errors, testing, logging

--- week09-testing/useFib.py
+++ week09-testing/useFib.py
@@ -1,6 +1,8 @@
 # useFib.py
 # prompt a user to enter a number and return the Fibonacci sequence up to that number
 
+# Authors: Andrew Beatty, David O'Connell
+
 import myFunctions
 
 num = int(input("Please enter a number of entries: "))
